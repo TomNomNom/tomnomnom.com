961d9b326bfca66a914860dfb60c4ea7639feb9b
Tom Hudson <mail@tomnomnom.com>
2015-06-14 19:14:42 -0400

    Pre-parses markdown; adds proper titles

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 type Blog struct {
 	DevMode  bool
 	Template *template.Template
-	Posts    map[string][]byte
+	Posts    map[string]Page
 }
 
 type Page struct {
@@ -24,60 +24,59 @@ type Page struct {
 }
 
 func (b *Blog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	post := ""
+	postName := ""
 	if r.URL.Path == "/" {
-		post = "index"
+		postName = "index"
 	} else {
-		post = r.URL.Path[7:]
+		postName = r.URL.Path[7:]
 	}
 
-	raw, ok := b.Posts[post]
+	post, ok := b.Posts[postName]
 	if !ok {
 		// Check for old-style links
-		post = strings.Replace(post, "_", "-", -1)
-		if _, ok := b.Posts[post]; ok {
-			http.Redirect(w, r, fmt.Sprintf("/posts/%s", post), http.StatusMovedPermanently)
+		postName = strings.Replace(postName, "_", "-", -1)
+		if _, ok := b.Posts[postName]; ok {
+			http.Redirect(w, r, fmt.Sprintf("/posts/%s", postName), http.StatusMovedPermanently)
 		} else {
 			http.Error(w, "404", http.StatusNotFound)
 		}
 		return
 	}
 
-	html := blackfriday.MarkdownCommon(raw)
-
-	b.Template.New("body").Parse(string(html))
-
-	p := &Page{
-		Title:   "Tom Hudson's blog - TomNomNom.com",
-		Body:    template.HTML(html),
-		DevMode: b.DevMode,
-	}
-	err := b.Template.Execute(w, p)
+	err := b.Template.Execute(w, post)
 	if err != nil {
 		log.Println(err)
 	}
 }
 
 func main() {
+	log.Println("Loading template...")
 	t, err := template.ParseFiles("templates/main.html")
 	if err != nil {
 		log.Fatal("Failed to load template file", err)
 	}
 
+	log.Println("Loading posts...")
 	dirs, err := ioutil.ReadDir("posts")
 	if err != nil {
 		log.Fatal("Failed to read posts", err)
 	}
-	posts := make(map[string][]byte)
+
+	posts := make(map[string]Page)
 	for _, dir := range dirs {
 		b, err := ioutil.ReadFile(fmt.Sprintf("posts/%s/article.mkd", dir.Name()))
 		if err != nil {
 			continue
 		}
-		posts[dir.Name()] = b
+		fistLine := strings.SplitN(string(b), "\n", 2)[0]
+		posts[dir.Name()] = Page{
+			Title:   fmt.Sprintf("%s - TomNomNom.com", strings.Trim(fistLine, "# ")),
+			Body:    template.HTML(blackfriday.MarkdownCommon(b)),
+			DevMode: true,
+		}
 	}
 
-	b := &Blog{DevMode: true, Template: t, Posts: posts}
+	b := &Blog{Template: t, Posts: posts}
 	http.Handle("/", b)
 
 	http.Handle("/styles/", http.FileServer(http.Dir("public")))
@@ -85,5 +84,6 @@ func main() {
 	http.Handle("/googlebfd35bb0eb2d4f45.html", http.FileServer(http.Dir("public")))
 	http.Handle("/keybase.txt", http.FileServer(http.Dir("public")))
 
+	log.Println("Starting up...")
 	http.ListenAndServe(":9090", nil)
 }
